f5b45bbdc12971113bef5a001576046aa62cf7aa
02Alexis <m_chael1012@hotmail.com>
2025-08-25 08:32:27 -0500

    creación de tema oscuro, mas confiduración para permanencia al recargar la pagina

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import assets from "../assets/assets";
+import ThemeToggleBtn from "./ThemeToggleBtn";
 
 const Navbar = ({theme, setTheme}) => {
     const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -19,6 +20,7 @@ const Navbar = ({theme, setTheme}) => {
         </div>
 
         <div className="flex items-center gap-2 sm:gap-4">
+            <ThemeToggleBtn theme={theme} setTheme={setTheme} />
 
             <img src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon} className="w-8 cursor-pointer sm:hidden" onClick={() => setSidebarOpen(true)} alt="" />
 
